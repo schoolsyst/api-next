e91fcfbde0ac1069bb7a6fd160fb454a37fd2edc
Ewen Le Bihan <ewen.lebihan7@gmail.com>
2020-07-26 17:10:01 +0200

    📝 Update documentation URLs
    SwaggerUI: /playground
    ReDoc: /

--- schoolsyst_api/main.py
+++ schoolsyst_api/main.py
@@ -6,12 +6,16 @@ from fastapi.responses import ORJSONResponse
 from schoolsyst_api import __version__, auth, cors
 
 # Extract some vars from pyproject.toml to use with FastAPI()
+SWAGGER_UI_DOCS_URL="/playground"
 pyproject = toml.load(open("pyproject.toml"))
 api = FastAPI(
     version=__version__,
     title="schoolsyst",
-    description=pyproject["tool"]["poetry"]["description"],
+    description=pyproject["tool"]["poetry"]["description"]
+    + f"\n\nInteractive (SwaggerUI) documentation at [{SWAGGER_UI_DOCS_URL}]({SWAGGER_UI_DOCS_URL})",
     default_response_class=ORJSONResponse,
+    docs_url=SWAGGER_UI_DOCS_URL,
+    redoc_url="/",
 )
 api.add_middleware(**cors.middleware_params)
 api.include_router(auth.router, tags=["Users"])
