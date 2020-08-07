115072877d40c42702fc1fcaa24733cdeb63a03c
Ewen Le Bihan <ewen.lebihan7@gmail.com>
2020-08-07 16:39:45 +0200

    🚧 Properly implement shortids
    - Fix .dict() not outputting the _key
    - For some reason, setting the default value for object_key always results in the same ID

--- schoolsyst_api/models.py
+++ schoolsyst_api/models.py
@@ -1,28 +1,20 @@
 from datetime import date, datetime, time
 from enum import Enum
-from typing import *
+from typing import Any, Dict, List, Literal, Optional, Set, Union
 
 import nanoid
-from pydantic import (
-    BaseModel,
-    EmailStr,
-    Field,
-    PositiveFloat,
-    PositiveInt,
-    confloat,
-    constr,
-)
+from pydantic import BaseModel as PydanticBaseModel
+from pydantic import EmailStr, Field, PositiveFloat, PositiveInt, confloat, constr
 from pydantic.color import Color
 from slugify import slugify
 
 Primantissa = confloat(le=1, ge=0)
-UsernameStr = constr(regex=r"[\w_-]+")
 ID_CHARSET = "abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789"
 USER_KEY_LEN = 10
 OBJECT_KEY_LEN = 6
 UserKey = constr(regex=f"[{ID_CHARSET}]{{{USER_KEY_LEN}}}")
 ObjectKey = constr(
-    regex=f"[{ID_CHARSET}]{{{USER_KEY_LEN}}}/[{ID_CHARSET}]{{{OBJECT_KEY_LEN}}}"
+    regex=f"[{ID_CHARSET}]{{{USER_KEY_LEN}}}:[{ID_CHARSET}]{{{OBJECT_KEY_LEN}}}"
 )
 ObjectBareKey = constr(regex=f"[{ID_CHARSET}]{{{OBJECT_KEY_LEN}}}")
 
@@ -31,8 +23,69 @@ def userkey():
     return nanoid.generate(ID_CHARSET, USER_KEY_LEN)
 
 
+def objectbarekey():
+    return nanoid.generate(ID_CHARSET, OBJECT_KEY_LEN)
+
+
 def objectkey(owner_key):
-    return f"{owner_key}/{nanoid.generate(ID_CHARSET, OBJECT_KEY_LEN)}"
+    return f"{owner_key}:{objectbarekey()}"
+
+
+class BaseModel(PydanticBaseModel):
+    """
+    Workaround for serializing properties with pydantic until
+    https://github.com/samuelcolvin/pydantic/issues/935
+    is solved
+
+    Credits: https://github.com/samuelcolvin/pydantic/issues/935#issuecomment-641175527
+    """
+
+    @classmethod
+    def get_properties(cls):
+        return [
+            prop
+            for prop in dir(cls)
+            if isinstance(getattr(cls, prop), property)
+            and prop not in ("__values__", "fields")
+        ]
+
+    def dict(
+        self,
+        *,
+        include: Set[str] = None,
+        exclude: Set[str] = None,
+        by_alias: bool = False,
+        skip_defaults: bool = None,
+        exclude_unset: bool = False,
+        exclude_defaults: bool = False,
+        exclude_none: bool = False,
+    ) -> Dict[str, Any]:
+        attribs = super().dict(
+            include=include,
+            exclude=exclude,
+            by_alias=by_alias,
+            skip_defaults=skip_defaults,
+            exclude_unset=exclude_unset,
+            exclude_defaults=exclude_defaults,
+            exclude_none=exclude_none,
+        )
+        include = include or set()
+        include |= {"_key"}
+        props = self.get_properties()
+        # Include and exclude properties
+        if include:
+            props = [prop for prop in props if prop in include]
+        if exclude:
+            props = [prop for prop in props if prop not in exclude]
+
+        # Update the attribute dict with the properties
+        if props:
+            attribs.update({prop: getattr(self, prop) for prop in props})
+
+        return attribs
+
+
+UsernameStr = constr(regex=r"[\w_-]+")
 
 
 class User(BaseModel):
@@ -57,19 +110,19 @@ class InUser(BaseModel):
     password: str
 
 
-class OwnedResouce(BaseModel):
+class OwnedResource(BaseModel):
     """
     Base model for resources owned by users
     """
 
-    object_key: ObjectBareKey = nanoid.generate(ID_CHARSET, OBJECT_KEY_LEN)
+    object_key: ObjectBareKey
     owner_key: UserKey
     updated_at: Optional[datetime] = None
     created_at: datetime = datetime.now()
 
     @property
     def _key(self) -> ObjectKey:
-        return objectkey(self.owner_key)
+        return f"{self.owner_key}:{self.object_key}"
 
 
 class DatetimeRange(BaseModel):
@@ -93,14 +146,14 @@ class WeekType(str, Enum):
     odd = "odd"
 
 
-class Settings(OwnedResouce):
+class Settings(OwnedResource):
     theme: ThemeName = ThemeName.auto
     """
     Configures how the year is split.
     For example, for a student whose school works on semesters, the layout will be
     [
         {start: <start of the year>, end: <end of the first semester>},
-        {start: <start of the 2nd semester, end: <end of the year>}
+        {start: <start of the 2nd semester>, end: <end of the year>}
     ]
     """
     year_layout: List[DateRange]
@@ -123,7 +176,7 @@ class Settings(OwnedResouce):
 class InSubject(BaseModel):
     name: str
     color: Color
-    weight: PositiveFloat = 1.0
+    weight: Union[PositiveFloat, Literal[0]] = 1.0
     goal: Primantissa
     room: str
 
@@ -132,11 +185,11 @@ class InSubject(BaseModel):
         return slugify(self.name)
 
 
-class Subject(InSubject, OwnedResouce):
+class Subject(InSubject, OwnedResource):
     pass
 
 
-class Quiz(OwnedResouce):
+class Quiz(OwnedResource):
     name: str
     # questions: List[Question] = []
     # Number of trials in test mode
@@ -170,7 +223,7 @@ class NoteContentType(str, Enum):
     mediawiki = "mediawiki"
 
 
-class Note(OwnedResouce):
+class Note(OwnedResource):
     name: str = ""
     content: str = ""
     type: NoteType = NoteType.html
@@ -181,7 +234,7 @@ class Note(OwnedResouce):
         return f"/notes/{self._key}/thubmnail"
 
 
-class Grade(OwnedResouce):
+class Grade(OwnedResource):
     title: str
     actual: Optional[Primantissa] = None
     expected: Optional[Primantissa] = None
@@ -198,7 +251,7 @@ class HomeworkType(str, Enum):
     exercise = "exercise"
 
 
-class Homework(OwnedResouce):
+class Homework(OwnedResource):
     title: str
     subject_key: ObjectKey
     type: HomeworkType
@@ -218,7 +271,7 @@ class ISOWeekDay(int, Enum):
     sunday = 7
 
 
-class Event(OwnedResouce):
+class Event(OwnedResource):
     subject_key: ObjectKey
     start: time
     end: time
@@ -264,7 +317,7 @@ class EventMutationInterpretation(str, Enum):
     deletion = "deletion"
 
 
-class EventMutation(OwnedResouce):
+class EventMutation(OwnedResource):
     event_key: Optional[ObjectKey] = None
     subject_key: Optional[ObjectKey] = None
     deleted_in: Optional[DateRange] = None
@@ -364,7 +417,7 @@ class EventMutation(OwnedResouce):
         return None
 
 
-class Course(OwnedResouce):
+class Course(OwnedResource):
     subject_key: ObjectKey
     start: datetime
     end: datetime
